7222f08cb94d769bbce784514bf02b9b1c1b407c
Neetesh Kumar <kumarneetesh24@gmail.com>
2016-03-17 15:50:06 +0530

    [FEAT] checking charger is connected or not to the system

--- notify_me.rb
+++ notify_me.rb
@@ -2,16 +2,23 @@ require 'libnotify'
 
 def read_charge
   battery_status = IO.popen("upower -i $(upower -e | grep BAT) | grep --color=never -E percentage|xargs|cut -d' ' -f2|sed s/%//")
-  return_status =  battery_status.readlines;
+  return_status =  battery_status.readlines
   battery_status.close
+  return return_status[0].to_i
+end
+
+def power_plugged_status
+  status = IO.popen("cat /sys/class/power_supply/ADP1/online")
+  return_status = status.readlines
+  status.close
   return return_status[0].to_i
 end
 
 def notify_status
   # using libnotify
   loop do
-    charge = read_charge
-    if charge == 78
+    if read_charge == 100  && power_plugged_status.nonzero?
+      p power_plugged_status
       note = Libnotify.new do |notify|
         notify.summary = "Battery 100%"
         notify.body = "Please remove the charger"
@@ -23,7 +30,7 @@ def notify_status
       end
       note.show!
     end
-    Kernel.sleep 20
+    Kernel.sleep 5
   end
 end
 
