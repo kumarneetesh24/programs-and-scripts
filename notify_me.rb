917ed915e4b7c36bc9ff6607a085ecc337220fc4
Neetesh Kumar <kumarneetesh24@gmail.com>
2016-03-18 17:14:52 +0530

    [FEAT] able to move to commandline

--- notify_me.rb
+++ notify_me.rb
@@ -1,3 +1,5 @@
+#!/usr/bin/env ruby
+
 require 'libnotify'
 
 def read_charge
@@ -30,7 +32,7 @@ def notify_status
       end
       note.show!
     end
-    Kernel.sleep 5
+    Kernel.sleep 20
   end
 end
 
